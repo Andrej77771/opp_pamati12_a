997a351465f29db9898554add01ab0d3d130f395
Andrej77771 <andrejsokolov63@gmail.com>
2024-09-16 09:25:06 +0300

    Update oop.py

--- oop.py
+++ oop.py
@@ -4,14 +4,10 @@ class person:
      self.age=age
  def output(self):
     print(self.name,self.age)
- def labdien(self):
-    print(f'Labdien, {self.name}!')
-
-p=person()
-p.set("Peter",20)
-p.output()
-d.set("Dzon",24)
-p.output()
-p.labdien()
-d.output()
-d.labdien()
+ p=person()
+ skaitlis=int(input('Ivadiet cilveku skaitu:'))
+ for i in range(skaits):
+    name=input('Ivadiet vecumu:')
+    age=int(input('Ivadiet vecumu:'))
+    person.set(p,name,age)
+    person.output(p)
